19139cb4232162b166b5e7b950e20ede50aba5e4
Robin T. Sverd <robin.sverd@gmail.com>
2026-05-03 19:38:11 +0000

    fix: dynamic base path for Vercel + SPA rewrite rules

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/low-effort-camp/',
+  base: process.env.VERCEL ? '/' : '/low-effort-camp/',
 })
